2af17cf03e65662a60aab82e25499884469dd8f5
Cube011806 <cube011806@gmail.com>
2026-04-08 04:10:41 +0200

    Porządkowanie przed pierwszą sesją

---
+++ RollMaster/Models/NPC.cs
@@ -0,0 +1,20 @@
+﻿namespace RollMaster.Models
+{
+    public class NPC
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty; //Imię
+        public int Endurance { get; set; } //Wytrzymałość
+        public int Might { get; set; } //Potęga
+        public int Hate { get; set; } //Nienawiść lub Determinacja (w zależności od IsHate)
+        public int Parry { get; set; } //Obrona
+        public int Armour { get; set; } //Pancerz
+        public int Attribute { get; set; } //Zajadłość
+        public bool IsHate { get; set; }
+        public string CombatAbilites { get; set; } = String.Empty; // Biegłości Bojowe
+        public string DarkAttributes { get; set; } = String.Empty; //Mroczne Atrybuty
+
+        //Brakuje relacji
+
+    }
+}
